87b1ed6a49252608f07899f720c88a2b540d3787
SharonIvy <kendishivy72@gmail.com>
2018-09-25 11:15:20 +0300

    Add quiz questions and their appropriate answers

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+// BUSINESS LOGIC:
+if (question1 == "false"){
+  correct++
+}
+if (question2 == "function myFunction()") {
+  correct++
+}
+if (question3 == "emcascript") {
+  correct++
+}
+if (question4 == "myFunction()") {
+  correct++
+}
+if (question5 == "if (i == 5)") {
+  correct++
+}
+if (question6 == "if (i != 5)") {
+  correct++
+}
+if (question7 == ".js") {
+  correct++
+}
+if (question8 == "script tag") {
+  correct++
+}
+if (question9 == "true") {
+  correct++
+}
+if (question10 == "false") {
+  correct++
+}
+var messages = ["100%", "90%", "80%", "70%", "60%", "50%", "40%", "30%", "20%", "10%", "0%"]
