03ab9ed1c52d84f5eda37e70525805dc4878a43f
SharonIvy <kendishivy72@gmail.com>
2018-09-25 13:47:37 +0300

    Add evaluation of score

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,85 @@
 // BUSINESS LOGIC:
+function check() {
+
+var question1 = document.quiz.question1.value;
+var question2 = document.quiz.question2.value;
+var question3 = document.quiz.question3.value;
+var question4 = document.quiz.question4.value;
+var question5 = document.quiz.question5.value;
+var question6 = document.quiz.question6.value;
+var question7 = document.quiz.question7.value;
+var question8 = document.quiz.question8.value;
+var question9 = document.quiz.question9.value;
+var question10 = document.quiz.question10.value;
+var correct = 0
+
 if (question1 == "false"){
-  correct++
+  correct++;
 }
 if (question2 == "function myFunction()") {
-  correct++
+  correct++;
 }
 if (question3 == "emcascript") {
-  correct++
+  correct++;
 }
 if (question4 == "myFunction()") {
-  correct++
+  correct++;
 }
 if (question5 == "if (i == 5)") {
-  correct++
+  correct++;
 }
 if (question6 == "if (i != 5)") {
-  correct++
+  correct++;
 }
 if (question7 == ".js") {
-  correct++
+  correct++;
 }
 if (question8 == "script tag") {
-  correct++
+  correct++;
 }
 if (question9 == "true") {
-  correct++
+  correct++;
 }
 if (question10 == "false") {
-  correct++
+  correct++;
 }
-var messages = ["100%", "90%", "80%", "70%", "60%", "50%", "40%", "30%", "20%", "10%", "0%"]
+
+var percentage = ["100%", "90%", "80%", "70%", "60%", "50%", "40%", "30%", "20%", "10%", "0%"];
+var score;
+if(correct < 1){
+  score = [0];
+}
+if (correct > 0 && correct<10){
+  score = [1];
+}
+if (correct > 0 && correct < 9) {
+  score = [2];
+}
+if (correct > 0 && correct < 8) {
+  score = [3];
+}
+if (correct > 0 && correct < 7) {
+  score = [4];
+}
+if (correct > 0 && correct < 6) {
+  score = [5];
+}
+if (correct > 0 && correct < 5) {
+  score = [6];
+}
+if (correct > 0 && correct < 4) {
+  score = [7];
+}
+if (correct > 0 && correct < 3) {
+  score = [8];
+}
+if (correct > 0 && correct < 2) {
+  score = [9];
+}
+if (correct > 0 && correct < 1) {
+  score = [10];
+}
+document.getElementById("score_reveal").style.visibility = "visible"
+document.getElementById("number_correct").innerHTML = "Your score is" + " " +  correct;
+document.getElementById("percentage").innerHTML = percentage[score];
+}
